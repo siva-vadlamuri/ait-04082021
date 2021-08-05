27d3d23434c5b795ffeb79b441cee7106962399d
sivavadlamuri <sivavadlamuru1997@gmail.com>
2021-08-05 09:48:25 +0530

    Operators in JavaScript

--- html/script.js
+++ html/script.js
@@ -3,4 +3,68 @@ console.log("JS is linked to HTML");
 
 // var firstName = ;
 var phone = "7777777";
-console.log(typeof phone);
+phone = true;
+phone = 76598;
+console.log(phone);
+
+// Boolean true/ false
+var isLogined = false;
+var isLoginedWithGoogle = true;
+
+// undefined
+
+var email;
+console.log(email);
+
+// null
+
+var firstName = null;
+// empty, no value
+
+firstName = "Mr";
+
+// Arrays , Array methods
+// String, String methods
+// Object
+
+// Arthematic Opertors
+
+var a = 2 + 3;
+// / %
+
+// 10/2
+var someNumber = 10 / 2;
+// console.log(someNumber);
+
+//  % Modulus
+var reminder = 10 % 2;
+// console.log(reminder);
+//
+
+// =, +=, -=,*=,/=, %=,
+
+var total = 10;
+// total = total + 320;
+total += 320;
+// console.log(total);
+
+var number = 5;
+// number = number * 5
+number *= 5;
+// console.log(number);
+
+// Logical Opertors && || !
+// condtional operators (Boolean) true,false
+// <,>,<=,>=,==,===
+console.log("Condtional Operators");
+// console.log(10 > 20);
+// console.log(10 < 10);
+
+// Equal to and strict equal to
+// ==
+// it will check the value but it will ignore the data type
+
+// console.log(10 == "10");
+// strict equal to it will check both value and data type
+// console.log('Shiva' === 'Shiva');
+console.log(false === 0);
