e9f337e87ef60216723f2d29a759dfca1d04529b
sivavadlamuri <sivavadlamuru1997@gmail.com>
2021-08-09 09:46:23 +0530

    loops-in-js

---
+++ javascript/loops.js
@@ -0,0 +1,54 @@
+// DRY Dont Repeat yourself!
+// alert("Hello");
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(1);
+// console.log(1);
+// console.log(1);
+// console.log(1);
+
+// Loops -->
+// for
+// while
+// do
+
+// es6
+// for in
+// for of
+
+// intilization (where to start) 10 9 8
+// condtion 100
+// increment or decrement
+
+// for(intilization;condtion;increment/decrement){}
+
+// Increment ++ / decrement Operator  --
+var a = 10;
+// increment is nothing adding one to the varibale
+a++;
+// console.log(a);
+
+// for (var i = 1; i < 10; i++) {
+//   console.log(i);
+// }
+
+// while
+var someNumber = 10;
+
+// while (someNumber < 100) {
+//   console.log(someNumber);
+//   someNumber++;
+// }
+//  do while
+for (var i = 1; i <= 100; i++) {
+  // all even number
+  //   if (i % 2 === 0) {
+  //     //   2 * 2 = 4
+  //     //  4 * 4 = 16
+  //     // 6 * 6 = 36
+  //     console.log(2 ** i);
+  //   }
+  console.log(2 ** i);
+}
