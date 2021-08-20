820897315797071965d699cf5b6b4662bcfb6515
sivavadlamuri <sivavadlamuru1997@gmail.com>
2021-08-20 09:41:08 +0530

    let-var-const

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,4 +1,6 @@
 //
+x();
+var globalVariable = "I am Global Variable";
 // Array methods
 var arr = [23, 78, 90];
 arr.push(88);
@@ -15,6 +17,12 @@ arr.push(88);
 
 // How to declare Function
 // function functionName
+
+// function loginUser(username,password){
+
+// }
+// loginUser('ait','123456')
+
 function greetTheUser(name = "John") {
   //parameters
   // body of the function
@@ -77,3 +85,77 @@ gstCalculation(10000, 20);
 gstCalculation(100000);
 gstCalculation(18000);
 gstCalculation(200000);
+
+// Scope
+// Difference between var , let and const
+// Difference between function declaration and function Expression
+
+// Var keyword was introduced on es5 (2009)
+
+// firstName = "Shiva"
+
+if (true) {
+  //   console.log(myName);
+  var myName = "Mr John";
+  //   Redeclaring
+  myName = "Marry";
+}
+
+console.log(myName);
+
+// let and const was introduced on es6 (ECMA 2015)
+// let and const are block scoped variable
+// Scope Accessibility of the data upto where we can access it
+// const block scoped variable
+// const --> constant
+// If you think your data will not change through out the application
+
+// Scope is Accessibility
+if (true) {
+  let firstName;
+  firstName = "Mr";
+  //   let firstName = "John";
+  //   console.log(firstName);
+  const randomAPI = "http://jsonplaceholder.typicode.com/";
+  console.log(randomAPI);
+}
+
+// When to go for let and when to go for const
+// if your data will change after some time (let)
+// if your data will not change through out the application (const)
+
+// global Scope (Entire File can acces the data)
+// Function Scope (You can access the data within the function)
+// Block Scope (We can access the data within the block)
+
+// if you declare a variable with var keyword inside the function then it will function scope
+// if you declare a variable with var keyword outside the function then it will global scope
+function myFunction() {
+  var email = "example@example.com";
+
+  if (true) {
+    let myRandomText = "I am random Text";
+    console.log(myRandomText);
+  }
+  //   console.log(myRandomText);
+}
+
+// console.log(email);
+myFunction();
+
+// Differenc between Function Declaration and Function Expression
+// Hoisting
+
+// Function Declaration
+function x() {
+  console.log("I am X");
+}
+
+// Function Expression
+
+const y = function () {
+  console.log("I am Y");
+};
+y();
+
+// Hoisting : We can access the function declaration before its declaration
